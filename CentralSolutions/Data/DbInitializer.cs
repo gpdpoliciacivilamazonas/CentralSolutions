ff9c9bd57f89bbdb5cf9378525003d0d17276be0
Lucas Martini <lucas.leite.martini@gmail.com>
2026-09-25 13:21:08 -0400

    feat: added env var for default user

--- CentralSolutions/Data/DbInitializer.cs
+++ CentralSolutions/Data/DbInitializer.cs
@@ -6,32 +6,32 @@ namespace CentralSolutions.Data;
 
 public static class DbInitializer
 {
-	public static async Task InitializeAsync(
-		ApplicationDbContext context,
-		IPasswordHasher<User> passwordHasher)
-	{
-		await context.Database.MigrateAsync();
+  public static async Task InitializeAsync(
+    ApplicationDbContext context,
+    IPasswordHasher<User> passwordHasher)
+  {
+    await context.Database.MigrateAsync();
 
-		if (await context.Users.AnyAsync())
-		{
-			return;
-		}
+    if (await context.Users.AnyAsync())
+    {
+      return;
+    }
 
-		var username = "admin";
-		var password = "admin123";
+    var username = Environment.GetEnvironmentVariable("APP_USERNAME");
+    var password = Environment.GetEnvironmentVariable("APP_PASSWORD");
 
-		var user = new User
-		{
-			Email = username,
-			IsActive = true
-		};
+    var user = new User
+    {
+      Email = username,
+      IsActive = true
+    };
 
-		user.PasswordHash = passwordHasher.HashPassword(
-			user,
-			password);
+    user.PasswordHash = passwordHasher.HashPassword(
+      user,
+      password);
 
-		context.Users.Add(user);
+    context.Users.Add(user);
 
-		await context.SaveChangesAsync();
-	}
+    await context.SaveChangesAsync();
+  }
 }
